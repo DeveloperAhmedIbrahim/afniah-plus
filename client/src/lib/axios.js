a9fdd27496f1567ac55be45548b1fdfe7f116648
Ahmed Ibrahim <siddiqui.ahmedibrahim@gmail.com>
2025-12-31 16:19:06 +0500

    updated

--- client/src/lib/axios.js
+++ client/src/lib/axios.js
@@ -3,9 +3,9 @@ import { toast } from 'sonner';
 import { clearFormErrors, extractFieldName } from './utils';
 
 const axiosInstance = axios.create({
-    // baseURL: 'https://afniah-plus.techrevivals.net/server/api',
+    baseURL: 'https://afniah-plus.techrevivals.net/server/api',
     // baseURL: 'http://localhost:8000/api',
-    baseURL: 'https://new.afnps.com/server/api',
+    // baseURL: 'https://new.afnps.com/server/api',
     timeout: 100000,
     headers: {
         'Content-Type': 'application/json',
--- client/src/lib/utils.js
+++ client/src/lib/utils.js
@@ -32,6 +32,6 @@ export const clearFormErrors = () => {
     });
 }
 
-// export const ASSETS_URL = "https://afniah-plus.techrevivals.net/server";
+export const ASSETS_URL = "https://afniah-plus.techrevivals.net/server";
 // export const ASSETS_URL = "http://localhost:8000";
-export const ASSETS_URL = "https://new.afnps.com/server";
+// export const ASSETS_URL = "https://new.afnps.com/server";
